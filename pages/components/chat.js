ec3bcf435065f0c1c9accc99e9598e89799d915f
Henrique <henrique.hfr2006@gmail.com>
2022-01-27 03:12:43 -0300

    main

--- pages/components/chat.js
+++ pages/components/chat.js
@@ -1,8 +1,208 @@
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import react from 'react';
+import AppConfig from '../../config.json';
 
-function paginaChat(){
-    return(
-        <div>olá mundo</div>
+export default function ChatPage() {
+    const [mensagem, setMensagem] = react.useState('')
+
+    const [listaMensagem, setListaMensagem] = react.useState([])
+
+    function handleNovaMensagem(novaMensagem) {
+        const mensagem = {
+            id: listaMensagem.length++,
+            de: 'rackn4r0k',
+            texto: novaMensagem,
+        }
+        setListaMensagem([
+            mensagem,
+            ...listaMensagem,
+        ])
+        setMensagem('')
+    }
+
+
+
+    // Sua lógica vai aqui
+
+    /* 
+    //Usuário
+    -escrever a mensagem no textarea
+    -irá enviar a mensagem quando o enter so precionado
+    -adicionar a mensagem no campo de mensagens
+
+
+    //dev
+    -[X]cria um campo de mensagens
+    -[X]ira usar um onChage e o useState e tem que ter um if para limpar a variavel depois de executada
+    -[ X]limpar o campo de mensagem(textarea)
+    */
+
+    // ./Sua lógica vai aqui
+    return (
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: AppConfig.theme.colors.primary[500],
+                backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: AppConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: AppConfig.theme.colors.neutrals[700],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: AppConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+                    <MessageList mensagens={listaMensagem} />
+                    {/* {listaMensagem.map((mensagemAtual) => {
+                        return (
+                            <li key={mensagemAtual.id}>
+                                {mensagemAtual.deQuem}: {mensagemAtual.texto} 
+                            </li>
+                        )
+                    })} */}
+
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            value={mensagem}
+                            onChange={(event) => {
+                                const valor = event.target.value
+                                setMensagem(valor)
+                            }}
+                            onKeyPress={(event) => {
+                                if (event.key === 'Enter') {
+                                    event.preventDefault()// faz quando a mensagem for enviada o campo volte ao normal e não na linha de baixo
+                                    handleNovaMensagem(mensagem)
+                                }
+
+                            }}
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: AppConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: AppConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
-export default paginaChat;
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log(props.mensagens)
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: AppConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {props.mensagens.map((mensagem) => {
+                return (
+                    <Text
+                        key={mensagem.id}
+                        tag="li"
+                        styleSheet={{
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: AppConfig.theme.colors.neutrals[700],
+                            }
+                        }}
+                    >
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/RACKN4R0K.png`}
+                            />
+                            <Text tag="strong">
+                                {mensagem.de}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: AppConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {mensagem.texto}
+                    </Text>
+                )
+            })}
+
+        </Box>
+    )
+}
